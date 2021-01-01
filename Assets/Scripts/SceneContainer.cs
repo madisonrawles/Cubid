d660d9550014855c4ffeaeb1faf53cdf272dc37d
Madison <maccmaddy@gmail.com>
2021-01-01 10:42:23 -0500

    added camera/gravity rotation

---
+++ Assets/Scripts/SceneContainer.cs
@@ -0,0 +1,59 @@
+﻿//This script is attached to the SceneContainer container object so that it can operate on the entire scene.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneContainer : MonoBehaviour
+{
+    private GameObject cam;
+    private GameObject player;
+    private Vector3[] gravity_angle_list = {new Vector3(0,-9.8f,0),new Vector3(9.8f,0,0),new Vector3(0,9.8f,0), new Vector3(-9.8f,0,0)};
+
+    public int angle_index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = transform.GetChild(0).gameObject;
+        player = transform.GetChild(1).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("RotateClockwise"))
+        {
+            RotateClockwise();
+        }
+        else if(Input.GetButtonDown("RotateCounterClockwise"))
+        {
+            RotateCounterClockwise();
+        }
+    }
+
+    void RotateClockwise()
+    {
+        //rotate camera by 90 degrees clockwise
+        Vector3 rotation = cam.transform.eulerAngles;
+        rotation.z += 90;
+        cam.transform.eulerAngles = rotation;
+        player.transform.eulerAngles = rotation;
+        //update gravity to match camera angles
+        angle_index = angle_index < gravity_angle_list.Length - 1 ? angle_index + 1 : 0;
+        player.GetComponent<basicMovement>().movement_index = angle_index;
+        Physics2D.gravity = gravity_angle_list[angle_index];
+    }
+
+    void RotateCounterClockwise()
+    {
+        //rotate camera by 90 degrees counterclockwise
+        Vector3 rotation = cam.transform.eulerAngles;
+        rotation.z -= 90;
+        cam.transform.eulerAngles = rotation;
+        player.transform.eulerAngles = rotation;
+        //update gravity to match camera angles
+        angle_index = angle_index > 0 ? angle_index - 1 : gravity_angle_list.Length - 1;
+        player.GetComponent<basicMovement>().movement_index = angle_index;
+        Physics2D.gravity = gravity_angle_list[angle_index];
+    }
+}
--- Assets/Scripts/basicMovement.cs
+++ Assets/Scripts/basicMovement.cs
@@ -5,69 +5,88 @@ using UnityEngine;
 
 public class basicMovement : MonoBehaviour
 {
-    //public vars so we can play with them in the inspector
+    //public vars so we can play with them in the inspector and/or edit them from outside scripts
     public float movement_scalar;
     public float jump_scalar;
     public float max_speed;
+    public int movement_index; 
 
 
     private Rigidbody2D rb;
     private bool is_jumping = false;
     private float x_movement;
     private bool is_on_ground = true;
+    private Vector2[] movement_angle_list = {new Vector2(1,0),new Vector2(0,1),new Vector2(-1,0), new Vector2(0,-1)};
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     //use this for collecting input and whatever else, *except* physics and movement
-    void Update(){
+    void Update()
+    {
         //jumping
         //we can set the Jump key in the Project Settings for this project
         //the default jump button is space
-        if(Input.GetButtonDown("Jump") && is_on_ground){
+        if(Input.GetButtonDown("Jump") && is_on_ground)
+        {
             is_jumping = true;
         }
+
+        x_movement = Input.GetAxis("Horizontal");
     }
 
     //use FixedUpdate for physics/movement since it is framerate-independent
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
+        //int gravityIndex = 
         //horizontal movement
-        x_movement = Input.GetAxis("Horizontal");
-        if(rb.velocity.magnitude < max_speed){
-            Vector2 movement = new Vector2(x_movement,0);
+        
+        if(rb.velocity.magnitude < max_speed)
+        {
+            Vector2 movement = x_movement * movement_angle_list[movement_index];
             //instead of adjusting position, we apply a force to the rigidbody to move it
             rb.AddForce(5.0f*movement_scalar * movement);
         }
         
         //jumping
-        if(is_jumping){
-            Vector2 jump_force = new Vector2(0,jump_scalar);
+        if(is_jumping)
+        {
+            int jump_index = movement_index < movement_angle_list.Length - 1 ? movement_index + 1 : 0;
+            Vector2 jump_angle = movement_angle_list[jump_index];
+            Vector2 jump_force = jump_scalar * jump_angle;
             rb.AddForce(jump_force);
             is_jumping = false;
         }
     }
 
     //runs when this object enters a collision
-    void OnCollisionEnter2D(Collision2D collision){
-        if(CollisionIsWithGround(collision)){
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(CollisionIsWithGround(collision))
+        {
             is_on_ground = true;
         }
     }
 
     //runs when this object exits a collision
-    void OnCollisionExit2D(Collision2D collision){
-        if(!CollisionIsWithGround(collision)){
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(!CollisionIsWithGround(collision))
+        {
             is_on_ground = false;
         }
     }
 
     //check to see if the collision passed to this method is with the ground
-    private bool CollisionIsWithGround(Collision2D collision){
+    private bool CollisionIsWithGround(Collision2D collision)
+    {
         bool is_with_ground = false;
         /*foreach(ContactPoint2D c in collision.contacts){
             Vector2 collision_direction_vector = c.point - rb.position;
